fcd1c5d212388f729bbcbc0b5ff61dc4df8ba83b
Leckar <gusleckar@gmail.com>
2022-09-23 15:43:31 -0300

    Login setup

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,1 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisabled, setDisabled] = useState(true);
+  return (
+    <div>
+      <input
+        name="email"
+        type="email"
+        data-testid="email-input"
+        value={ email }
+        placeholder="Digite seu e-mail"
+      />
+      <input
+        name="password"
+        type="password"
+        data-testid="password-input"
+        value={ password }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ isDisabled }
+      >
+        Enter
+      </button>
+    </div>
+  );
+}
