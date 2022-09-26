20e622a54373e776a81b2e49b4fc1a6f445584f6
Leckar <gusleckar@gmail.com>
2022-09-26 18:05:25 -0300

    Tests pending

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,49 @@
-Eoq;
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import styles from './Footer.module.css';
+
+const PATHNAMES = ['/meals', '/drinks', '/profile'];
+
+function Footer() {
+  const history = useHistory();
+  const [isRendering, setIsRendering] = useState(false);
+  const { location: { pathname } } = history;
+
+  useEffect(() => {
+    if (PATHNAMES.includes(pathname)) {
+      setIsRendering(true);
+    } else {
+      setIsRendering(false);
+    }
+  }, [pathname]);
+
+  if (isRendering) {
+    return (
+      <footer
+        data-testid="footer"
+        className={ styles.footer }
+      >
+        <Link to="/meals">
+          <img
+            src={ drinkIcon }
+            alt="Ícone de bebidas"
+            data-testid="drinks-bottom-btn"
+          />
+        </Link>
+        <Link to="/drinks">
+
+          <img
+            src={ mealIcon }
+            alt="Ícone de refeições"
+            data-testid="meals-bottom-btn"
+          />
+        </Link>
+      </footer>
+    );
+  }
+  return null;
+}
+
+export default Footer;
