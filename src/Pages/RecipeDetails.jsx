c45edf32c792eb2c6d106c575d21a75659db2809
Matheus Nascimento <nasc.matheusfrancisco@gmail.com>
2022-09-28 16:48:23 -0300

    cumprindo o requisito 27

--- src/Pages/RecipeDetails.jsx
+++ src/Pages/RecipeDetails.jsx
@@ -1,26 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, withRouter } from 'react-router-dom';
 import fetchRecipeDetails from '../utils/fetchRecipeDetails';
 import fetchToRecipes from '../utils/fetchToRecipes';
 import styles from './RecipeDetails.module.css';
 
 function RecipeDetails() {
-  const { location: { pathname } } = useHistory();
+  const history = useHistory();
   const [details, setDetails] = useState({});
   const [loading, setLoading] = useState(true);
+  const [recommendedRecipes, setRecommendedRecipes] = useState([]);
+  const [recommendedRecipe, setRecommendedRecipe] = useState({});
 
-  const recipeInfo = pathname.split('/');
+  const recipeInfo = history.location.pathname.split('/');
+  const recomendedArrayLength = 6;
+
+  useEffect(() => {
+    if (loading && Object.keys(recommendedRecipe).length > 0) {
+      history.push(`../${recipeInfo[1] === 'meals' ? 'drinks' : 'meals'}/${
+        recipeInfo[1] === 'meals'
+          ? recommendedRecipe.idDrink : recommendedRecipe.idMeal}`);
+    }
+  }, [loading, recommendedRecipe]);
 
   useEffect(() => {
     const getDetails = async () => {
       const result = await fetchRecipeDetails(recipeInfo[1], recipeInfo[2]);
       setDetails({ ...result });
       const type = (recipeInfo[1] === 'meals') ? 'drinks' : 'meals';
-      const recomendedRecipe = await fetchToRecipes(type);
-      console.log(recomendedRecipe);
+      const recipes = await fetchToRecipes(type);
+      const recomended = recipes.filter((_, index) => index < recomendedArrayLength);
+      setRecommendedRecipes(recomended);
     };
     getDetails();
-  }, []);
+  }, [history.location.pathname]);
 
   useEffect(() => {
     if (Object.keys(details).length > 1) {
@@ -28,8 +40,13 @@ function RecipeDetails() {
     }
   }, [details]);
 
+  const handleRecomended = (recipe) => {
+    setRecommendedRecipe(recipe);
+    setLoading(true);
+  };
+
   return (
-    <main>
+    <main className={ styles.container }>
       { loading ? <span>carregando...</span> : (
         <>
           <img
@@ -80,10 +97,32 @@ function RecipeDetails() {
               data-testid="video"
             />
           ) }
+          <ul className={ styles.recommendations }>
+            { recommendedRecipes.map((recipe, index) => (
+              <li
+                key={ index }
+                className={ styles.recommendation }
+                data-testid={ `${index}-recommendation-card` }
+              >
+                <button type="button" onClick={ () => handleRecomended(recipe) }>
+                  <img
+                    src={
+                      recipe[(recipeInfo[1] === 'meals'
+                        ? 'strDrinkThumb' : 'strMealThumb')]
+                    }
+                    alt=""
+                  />
+                  <p data-testid={ `${index}-recommendation-title` }>
+                    {recipe[(recipeInfo[1] === 'meals' ? 'strDrink' : 'strMeal')]}
+                  </p>
+                </button>
+              </li>
+            )) }
+          </ul>
         </>
       ) }
     </main>
   );
 }
 
-export default RecipeDetails;
+export default withRouter(RecipeDetails);
