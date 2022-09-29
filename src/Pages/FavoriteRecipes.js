8a61649655b78381ad27524dcae898f4d6226339
Matheus Nascimento <nasc.matheusfrancisco@gmail.com>
2022-09-29 15:26:56 -0300

    cumprindo requisitos 55 e 56 (sem testes)

--- src/Pages/FavoriteRecipes.js
+++ src/Pages/FavoriteRecipes.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import shareIcon from '../images/shareIcon.svg';
 import favoritedIcon from '../images/blackHeartIcon.svg';
+import styles from './FavoriteRecipes.module.css';
 
 const copy = require('clipboard-copy');
 
 function FavoriteRecipes() {
   const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  const [favoriteRecipesToShow, setFavoriteRecipesToShow] = useState([]);
   const [linkCopied, setLinkCopied] = useState('');
 
   useEffect(() => {
     const favoriteRecipeStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
     setFavoriteRecipes(favoriteRecipeStorage);
+    setFavoriteRecipesToShow(favoriteRecipeStorage);
     setLinkCopied('');
   }, []);
 
@@ -23,7 +27,15 @@ function FavoriteRecipes() {
   const handleUnfavorite = (id) => {
     const newFavoriteRecipes = favoriteRecipes.filter((recipe) => recipe.id !== id);
     setFavoriteRecipes(newFavoriteRecipes);
+    setFavoriteRecipesToShow(newFavoriteRecipes);
     localStorage.setItem('favoriteRecipes', JSON.stringify(newFavoriteRecipes));
+  };
+
+  const handleFilter = (type) => {
+    if (type) {
+      const filteredRecipes = favoriteRecipes.filter((recipe) => recipe.type === type);
+      setFavoriteRecipesToShow(filteredRecipes);
+    } else setFavoriteRecipesToShow(favoriteRecipes);
   };
 
   return (
@@ -32,36 +44,42 @@ function FavoriteRecipes() {
         <button
           data-testid="filter-by-all-btn"
           type="button"
+          onClick={ () => handleFilter() }
         >
           All
         </button>
         <button
           data-testid="filter-by-meal-btn"
           type="button"
+          onClick={ () => handleFilter('meal') }
         >
           Meal
         </button>
         <button
           data-testid="filter-by-drink-btn"
           type="button"
+          onClick={ () => handleFilter('drink') }
         >
           Drink
         </button>
       </section>
       <section>
         <ul>
-          { favoriteRecipes.map((recipe, index) => (
+          { favoriteRecipesToShow.map((recipe, index) => (
             <li key={ index }>
-              <img
-                data-testid={ `${index}-horizontal-image` }
-                src={ recipe.image }
-                alt=""
-              />
-              <span data-testid={ `${index}-horizontal-name` }>{ recipe.name }</span>
-              <span data-testid={ `${index}-horizontal-top-text` }>
-                { `${recipe.nationality} - ${recipe.category}` }
-                { recipe.type === 'drink' && <p>{ recipe.alcoholicOrNot }</p> }
-              </span>
+              <Link to={ `../${recipe.type}s/${recipe.id}` }>
+                <img
+                  data-testid={ `${index}-horizontal-image` }
+                  src={ recipe.image }
+                  alt=""
+                  className={ styles.favorite_image }
+                />
+                <span data-testid={ `${index}-horizontal-name` }>{ recipe.name }</span>
+                <span data-testid={ `${index}-horizontal-top-text` }>
+                  { `${recipe.nationality} - ${recipe.category}` }
+                  { recipe.type === 'drink' && <p>{ recipe.alcoholicOrNot }</p> }
+                </span>
+              </Link>
               <button
                 type="button"
                 onClick={ () => handleShare(recipe.id, recipe.type) }
